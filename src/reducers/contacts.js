d3f3918f5461012a9880e2abe104d39d2a0810a5
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-20 17:57:16 -0500

    Feature: Add function to reducer to handle add new contact and create id

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,8 +1,24 @@
+import reduxUtils from '../../libs/Redux-Utility-Functions';
+
 const initalState = {
   list: [],
 };
+
+function addContactToList(list, contact) {
+  const listIds = reduxUtils.map(list, each => each.id);
+  const maxId = reduxUtils.max(listIds);
+  const contactWithId = reduxUtils.merge({ id: maxId + 1 }, contact);
+
+  return reduxUtils.take(list, list.length).concat(contactWithId);
+}
+
 const contacts = (state = initalState, action) => {
   switch (action.type) {
+    case 'ADD_CONTACT':
+      return {
+        ...state,
+        list: addContactToList(state.list, action.contact),
+      };
     default:
       return state;
   }
