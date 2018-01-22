d79c6e839694e93429e29a26e393226ddb0a638a
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-22 17:03:34 -0500

    Feature: Update Contact Item component with state to display edit modal

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -3,26 +3,45 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { removeContact } from '../actions/contactActions';
 
-function ContactItem({ contact, sortBy, deleteContact }) {
-  const { name, phone, email } = contact;
-  return (
-    <li>
-      <i className="fa fa-minus-square remove-btn" aria-hidden="true" onClick={() => deleteContact(contact)} />
-      <div className="name">
-        {sortBy === 'LAST' ? (
-          `${name.last}, ${name.first}`
-        ) : (
-          `${name.first} ${name.last}`
+class ContactItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editModal: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      editModal: !this.state.editModal,
+    });
+  }
+
+  render() {
+    const { sortBy, contact, deleteContact } = this.props;
+    const { name, phone, email } = contact;
+    return (
+      <li>
+        <i className="fa fa-minus-square remove-btn" aria-hidden="true" onClick={() => deleteContact(contact)} />
+        <i className="fa fa-pencil-square edit-btn" aria-hidden="true" onClick={this.handleClick} type="edit" />
         )}
-      </div>
-      <div className="phone">
-        {phone}
-      </div>
-      <div className="email">
-        {email}
-      </div>
-    </li>
-  );
+        <div className="name">
+          {sortBy === 'LAST' ? (
+            `${name.last}, ${name.first}`
+          ) : (
+            `${name.first} ${name.last}`
+          )}
+        </div>
+        <div className="phone">
+          {phone}
+        </div>
+        <div className="email">
+          {email}
+        </div>
+      </li>
+    );
+  }
 }
 
 ContactItem.propTypes = {
